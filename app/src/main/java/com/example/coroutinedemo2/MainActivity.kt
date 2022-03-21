5c3b07535037a6daf2df22d8daff26cf3102b642
sameeksha <sameeksha,mourya@coditas.com>
2022-03-21 11:25:20 +0530

    Synchronous network call without async

--- app/src/main/java/com/example/coroutinedemo2/MainActivity.kt
+++ app/src/main/java/com/example/coroutinedemo2/MainActivity.kt
@@ -3,27 +3,29 @@ package com.example.coroutinedemo2
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val job = GlobalScope.launch(Dispatchers.Default) {
-            Logger.infoLog("Starting long running Calculation...")
-            withTimeout(3000L) {
-                for (i in 30..40) {
-                    if (isActive) {
-                        Logger.infoLog("Result for i=$i: ${fib(i)}")
-                    }
-                }
+        GlobalScope.launch(Dispatchers.IO) {
+            val time= measureTimeMillis {
+                val answer1=networkCall1()
+                val answer2=networkCall2()
+                Logger.infoLog("Answer1 is $answer1")
+                Logger.infoLog("Answer2 is $answer2")
             }
-            Logger.infoLog("Ending long running Calculation...")
+            Logger.infoLog("Requests took $time ms.")
         }
     }
-
-    fun fib(n: Int): Long {
-        return if (n == 0) 0
-        else if (n == 1) 1
-        else fib(n - 1) + fib(n - 2)
+    suspend fun networkCall1():String{
+       delay(3000L)
+        return "Answer 1"
+    }
+    suspend fun networkCall2():String{
+        delay(3000L)
+        return "Answer 2"
     }
 }
+
